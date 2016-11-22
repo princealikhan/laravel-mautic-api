d62f38b011f18a97a27894cb4865fd91677e91fe
Prince Ali Khan <prince@zoojoo.be>
2016-11-23 00:56:02 +0530

    service provider created

--- src/MauticServiceProvider.php
+++ src/MauticServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Princealikhan\Mautic;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class MauticServiceProvider extends ServiceProvider {
@@ -13,21 +14,66 @@ class MauticServiceProvider extends ServiceProvider {
 	{
 		// Publish Configuration File to base Path.
         $this->publishes([
-            __DIR__.'/config/config.php' => base_path('config/mautic.php'),
+            __DIR__.'/config/mautic.php' => base_path('config/mautic.php'),
         ]);
 	}
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerFactory($this->app);
+        $this->registerManager($this->app);
+    }
 
-		$this->app->bind('mautic', function () {
-    			return new Mautic;
-			});
-	}
+    /**
+     * Register the factory class.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function registerFactory(Application $app)
+    {
+        $app->singleton('mautic.factory', function () {
+            return new Factories\MauticFactory();
+        });
 
+        $app->alias('mautic.factory', 'Princealikhan\Mautic\Factories\MauticFactory');
+    }
+
+    /**
+     * Register the manager class.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function registerManager(Application $app)
+    {
+        $app->singleton('mautic', function ($app) {
+            $config = $app['config'];
+            $factory = $app['mautic.factory'];
+
+            return new Mautic($config, $factory);
+        });
+
+        $app->alias('mautic', 'Princealikhan\Mautic\Mautic');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'mautic',
+            'mautic.factory',
+        ];
+    }
 }
