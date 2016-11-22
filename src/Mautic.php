fa13d8526c265fb46028c389261dd1775209db7e
Prince Ali Khan <prince@zoojoo.be>
2016-11-23 00:59:11 +0530

    Mautic

--- src/Mautic.php
+++ src/Mautic.php
@@ -1,12 +1,68 @@
-<?php namespace Princealikhan\Mautic\;
+<?php namespace Princealikhan\Mautic;
 
 use Princealikhan\Mautic\Factories\MauticFactory;
-use Illuminate\Support\Facades\Config;
+use GrahamCampbell\Manager\AbstractManager;
+use Illuminate\Contracts\Config\Repository;
+use Mautic\Auth\OAuthClient;
 
-class Mautic extends MauticFactory{
-	
-	function __construct()
-	{
-		
-	}
+class Mautic extends AbstractManager
+{
+
+    /**
+     * The factory instance.
+     *
+     * @var \Mautic\Factory
+     */
+    protected $factory;
+
+    /**
+     * Create a new Mautic manager instance.
+     *
+     * @param $config
+     * @param $factory
+     *
+     * @return void
+     */
+    public function __construct(Repository $config, MauticFactory $factory)
+    {
+        parent::__construct($config);
+
+        $this->factory = $factory;
+    }
+
+    /**
+     * Create the connection instance.
+     *
+     * @param array $config
+     *
+     * @return mixed
+     */
+    protected function createConnection(array $config)
+    {
+        return $this->factory->make($config);
+    }
+
+    /**
+     * Get the configuration name.
+     *
+     * @return string
+     */
+    protected function getConfigName()
+    {
+        return 'mautic';
+    }
+
+    /**
+     * Get the factory instance.
+     *
+     * @return \Mautic\MauticFactory
+     */
+    public function getFactory()
+    {
+        return $this->factory;  
+    }
+
 }
+
+
+
