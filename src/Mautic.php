955a3d401b91d96ee6641251a62c9d64d6414f9a
Prince Ali Khan <prince@zoojoo.be>
2016-11-26 14:19:22 +0530

    request api call

--- src/Mautic.php
+++ src/Mautic.php
@@ -4,6 +4,7 @@ use Princealikhan\Mautic\Factories\MauticFactory;
 use GrahamCampbell\Manager\AbstractManager;
 use Illuminate\Contracts\Config\Repository;
 use Mautic\Auth\OAuthClient;
+use Princealikhan\Mautic\Models\MauticConsumer;
 
 class Mautic extends AbstractManager
 {
@@ -60,6 +61,29 @@ class Mautic extends AbstractManager
     public function getFactory()
     {
         return $this->factory;  
+    }
+
+    /**
+     * @param null $method
+     * @param null $endpoints
+     * @param null $body
+     * @return mixed
+     */
+    public function request($method=null, $endpoints=null, $body=null)
+    {
+
+        $consumer = MauticConsumer::whereNotNull('id')
+            ->orderBy('created_at', 'desc')
+            ->first();
+
+        $expirationStatus = $this->factory->checkExpirationTime($consumer->expires);
+
+        if($expirationStatus==true){
+            $newToken   =  $this->factory->refreshToken($consumer->refresh_token);
+            return $this->factory->callMautic($method,$endpoints,$body,$newToken->access_token);
+        } else{
+            return $this->factory->callMautic($method,$endpoints,$body,$consumer->access_token);
+        }
     }
 
 }
