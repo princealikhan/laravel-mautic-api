74e3ab38b78899097fa88447052f47dc4874ae78
Prince Ali Khan <princealikhan08@gmail.com>
2017-11-11 14:28:12 +0530

    Config enhancement and code cleanup

--- src/Factories/MauticFactory.php
+++ src/Factories/MauticFactory.php
@@ -1,8 +1,58 @@
 <?php namespace Princealikhan\Mautic\Factories;
 
-use Illuminate\Support\Facades\Config;
+use Mautic\Auth\OAuthClient;
+
 class MauticFactory
 {
-    
 
+    /**
+     * Make a new Mautic client.
+     *
+     * @param array $config
+     * @return \Mautic\Mautic
+     */
+    public function make(array $config)
+    {
+        $config = $this->getConfig($config);
+
+        return $this->getClient($config);
+    }
+
+    /**
+     * Get the configuration data.
+     *
+     * @param string[] $config
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return array
+     */
+    protected function getConfig(array $config)
+    {
+        $keys = ['clientKey', 'clientSecret'];
+
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $config)) {
+                throw new \InvalidArgumentException('The Mautic client requires configuration.');
+            }
+        }
+
+        return array_only($config, ['client_id', 'client_secret', 'access_token']);
+    }
+
+    /**
+     * Get the Mautic client.
+     *
+     * @param array $auth
+     *
+     * @return \Mautic\MauticApi
+     */
+    protected function getClient(array $auth)
+    {
+        return new OAuthClient(
+            $auth['public_key'],
+            $auth['secret_key'],
+            $auth['access_token']
+        );
+    }
 }
--- src/Mautic.php
+++ src/Mautic.php
@@ -1,12 +1,161 @@
-<?php namespace Princealikhan\Mautic\;
+<?php namespace Princealikhan\Mautic;
 
 use Princealikhan\Mautic\Factories\MauticFactory;
-use Illuminate\Support\Facades\Config;
+use GrahamCampbell\Manager\AbstractManager;
+use Illuminate\Contracts\Config\Repository;
+use Mautic\Auth\ApiAuth;
+use Mautic\Auth\OAuthClient;
 
-class Mautic extends MauticFactory{
-	
-	function __construct()
-	{
-		
-	}
+
+
+class Mautic extends AbstractManager
+{
+
+    /**
+     * The factory instance.
+     *
+     * @var \Vinkla\Vimeo\Factories\VimeoFactory
+     */
+    protected $factory;
+
+    /**
+     * Create a new Vimeo manager instance.
+     *
+     * @param $config
+     * @param $factory
+     *
+     * @return void
+     */
+    public function __construct(Repository $config, MauticFactory $factory)
+    {
+        parent::__construct($config);
+
+        $this->factory = $factory;
+    }
+
+    /**
+     * Create the connection instance.
+     *
+     * @param array $config
+     *
+     * @return mixed
+     */
+    protected function createConnection(array $config)
+    {
+        return $this->factory->make($config);
+    }
+
+    /**
+     * Get the configuration name.
+     *
+     * @return string
+     */
+    protected function getConfigName()
+    {
+        return 'mautic';
+    }
+
+    /**
+     * Get the factory instance.
+     *
+     * @return \Vinkla\Vimeo\Factories\VimeoFactory
+     */
+    public function getFactory()
+    {
+        return $this->factory;  
+    }
+
+    public function test()
+    {
+        session_name("oauthtester");
+        session_start();
+        $callback= 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $settings = array(
+            'base_url'      => env('MAUTIC_BASE_URL','http://mautic.dev'),
+            'clientKey'    => env('MAUTIC_PUBLIC_KEY','5onittxoj30gwco0csc4g4sk4okckwgsg448wkcw0okoscgkwo'),
+            'clientSecret'    => env('MAUTIC_SECRET_KEY','zq1a54bmcis4ggkkkgwswcs4kk8k4ko0cs8sgkgkok4wg8cwo'),
+            'callback'      => env('MAUTIC_CALLBACK',$callback),
+
+            'version'          => 'OAuth1a', // Version of the OAuth can be OAuth2 or OAuth1a. OAuth2 is the default value.
+
+        );
+
+
+        // If you already have the access token, et al, pass them in as well to prevent the need for reauthorization
+//        $settings['accessToken']        = $accessToken;
+//        $settings['accessTokenSecret']  = $accessTokenSecret; //for OAuth1.0a
+//        $settings['accessTokenExpires'] = $accessTokenExpires; //UNIX timestamp
+//        $settings['refreshToken']       = $refreshToken;
+
+
+        // Initiate the auth object
+        $auth = \Mautic\Auth\ApiAuth::initiate($settings);
+
+        return $auth->validateAccessToken();
+//        $initAuth = new ApiAuth();
+//        $auth = $initAuth->newAuth($settings);
+
+
+        try {
+            if ($auth->validateAccessToken()) {
+
+                // Obtain the access token returned; call accessTokenUpdated() to catch if the token was updated via a
+                // refresh token
+
+                // $accessTokenData will have the following keys:
+                // For OAuth1.0a: access_token, access_token_secret, expires
+                // For OAuth2: access_token, expires, token_type, refresh_token
+
+                if ($auth->accessTokenUpdated()) {
+                    return $accessTokenData = $auth->getAccessTokenData();
+
+                    //store access token data however you want
+                }
+            }
+        } catch (Exception $e) {
+            // Do Error handling
+            return $e;
+        }
+
+    }
+//	public function test(){
+//        $settings = array(
+//            'baseUrl'          => 'http://mautic.dev',       // Base URL of the Mautic instance
+//            'version'          => 'OAuth', // Version of the OAuth can be OAuth2 or OAuth1a. OAuth2 is the default value.
+//            'clientKey'        => '5onittxoj30gwco0csc4g4sk4okckwgsg448wkcw0okoscgkwo',       // Client/Consumer key from Mautic
+//            'clientSecret'     => 'zq1a54bmcis4ggkkkgwswcs4kk8k4ko0cs8sgkgkok4wg8cwo',       // Client/Consumer secret key from Mautic
+//            'callback'         => 'http://mautic.dev/callback'        // Redirect URI/Callback URI for this script
+//        );
+//
+//
+//        $auth =   \Mautic\Auth\ApiAuth::initiate(array('OAuth'));
+//
+//        $auth->newAuth($settings);
+//        try {
+//            if ($auth->validateAccessToken()) {
+//
+//                // Obtain the access token returned; call accessTokenUpdated() to catch if the token was updated via a
+//                // refresh token
+//
+//                // $accessTokenData will have the following keys:
+//                // For OAuth1.0a: access_token, access_token_secret, expires
+//                // For OAuth2: access_token, expires, token_type, refresh_token
+//
+//                if ($auth->accessTokenUpdated()) {
+//                    $accessTokenData = $auth->getAccessTokenData();
+//
+//                    //store access token data however you want
+//                }
+//            }
+//        } catch (Exception $e) {
+//            echo 'saldfjksld';
+//
+//            // Do Error handling
+//        }
+//        dd($auth);
+//		return array('test' => 'asdf');;
+//	}
 }
+
+
+
